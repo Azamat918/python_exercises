30ea7cbab7653084a741cc7e92241454447e70c0
Azamat918 <k.azamat918@gmail.com>
2020-07-28 18:37:58 +0600

    littlebit fixes

--- parsing_articles.py
+++ parsing_articles.py
@@ -7,6 +7,7 @@ from time import sleep
 ATTEMPTS = 4
 SLEEP_SEC = 4
 MAIN_LINK = "https://vk.com"
+YVKURSE_LINK = "https://vk.com/@yvkurse"
 LOAD_MORE_LINK = "https://vk.com/al_articles.php"
 CSV_FILE_DIRECTORY = 'parsing_result.csv'
 LOGGING_FILE_DIRECTORY = 'parsing_articles.log'
@@ -47,22 +48,15 @@ def get_article_links():
         'sec-fetch-dest': 'empty',
         'referer': 'https://vk.com/^@yvkurse',
         'accept-language': 'ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7',
-        'cookie': "remixscreen_depth=24; tmr_lvid=d49c52184f0d7500227e8e4a29e38621; tmr_lvidTS=1576690516601; "
-                  "remixstid=1552952741_bm2JOQMGsRL6NRPARJjP3KhncI7NYBzuNI2X8XpDlWw; remixscreen_width=1536; "
-                  "remixscreen_height=864; remixscreen_dpr=1.25; remixlang=0; "
-                  "remixusid=NTFhNzEyYTdhMTdjOTM0ODIxMzA3MjA4; remixflash=0.0.0; remixab=1; remixscreen_orient=1; "
-                  "remixdt=10800; remixgp=7365d6dbb84c021f98ef81350a596c22; remixrefkey=da25fc1f881e6cc5bf; "
-                  "remixua=-1^%^7C-1^%^7C174^%^7C1746444182; remixseenads=0; "
-                  "remixsid=c227a8ced4e82eb0ecf84672ad436a3a52ebee877d492a2011a02f1ad96f5; "
-                  "remixscreen_winzoom=2.13; tmr_detect=1^%^7C1595542513347; tmr_reqNum=256; "
-                  "remixjsp=^%^7B^%^22id^%^22^%^3A^%^22kd34sh8j.z7^%^22^%^2C^%^22loc^%^22^%^3A^%^22https^%^3A/"
-                  "/vk.com/^@yvkurse^%^22^%^2C^%^22events^%^22^%^3A^%^5B^%^5B^%^22TTFCP^%^22^%^2C781^%^2C^%^22a"
-                  "uthor_page^%^22^%^2C^%^224g^%^22^%^2C^%^2225452^%^22^%^5D^%^2C^%^5B^%^22domComplete^%^22^%^2"
-                  "C1895^%^2C^%^22author_page^%^22^%^2C^%^224g^%^22^%^2C^%^2225452^%^22^%^5D^%^2C^%^5B^%^22domCo"
-                  "ntentLoadedEventEnd^%^22^%^2C1024^%^2C^%^22author_page^%^22^%^2C^%^224g^%^22^%^2C^%^2225452^%"
-                  "^22^%^5D^%^2C^%^5B^%^22loadEventEnd^%^22^%^2C1895^%^2C^%^22author_page^%^22^%^2C^%^224g^%^22"
-                  "^%^2C^%^2225452^%^22^%^5D^%^2C^%^5B^%^22TTI^%^22^%^2C4132^%^2C^%^22author_page^%^22^%^2C^%^22"
-                  "4g^%^22^%^2C^%^2225452^%^22^%^5D^%^5D^%^7D",
+        'cookie': "remixscreen_depth=24; tmr_lvid=d49c52184f0d7500227e8e4a29e38621; "
+                  "tmr_lvidTS=1576690516601; remixstid=1552952741_bm2JOQMGsRL6NRPARJjP3KhncI7NYBzuNI2X8XpDlWw; "
+                  "remixscreen_width=1536; remixscreen_height=864; remixscreen_dpr=1.25; remixlang=0; "
+                  "remixusid=NTFhNzEyYTdhMTdjOTM0ODIxMzA3MjA4; remixflash=0.0.0; remixab=1; "
+                  "remixscreen_orient=1; remixua=-1%7C-1%7C174%7C1746444182; remixseenads=0; "
+                  "remixrefkey=b6dfe967838768fed1; "
+                  "remixsid=37245ef0e49f394c658a2a07aa72bdb5d6c124707e220303c2976a19c0f80; "
+                  "remixgp=7365d6dbb84c021f98ef81350a596c22; remixdt=-10800; remixscreen_winzoom=1.52; "
+                  "tmr_detect=1%7C1595938289060; tmr_reqNum=290",
     }
     data = {
         'act': 'author_page_more',
@@ -83,26 +77,27 @@ def get_article_links():
             try:
                 name_url = {'url': MAIN_LINK + soup.a['href'],
                             'name': soup.find('div', class_='author_page_article_title').text}
+                data['articles_offset'] = '1'
+                urls.append(name_url)
+                continue
             except Exception as e:
                 logging.warning(e)
-                name_url = {}
-            data['articles_offset'] = '1'
-            urls.append(name_url)
+                return None
+        page_grid_titles = soup.find_all('div', class_='author_page_grid_article')
+        if page_grid_titles is None or len(page_grid_titles) == 0:
+            break
         else:
-            page_grid_titles = soup.find_all('div', class_='author_page_grid_article')
-            if page_grid_titles is None or len(page_grid_titles) == 0:
-                break
-            else:
+            if len(page_grid_titles) > 0:
                 for page_grid_title in page_grid_titles:
                     try:
                         title = page_grid_title.a.find('div', class_='author_page_article_title')
                         name_url = {'url': MAIN_LINK + page_grid_title.a['href'],
                                     'name': title.text}
+                        urls.append(name_url)
                     except Exception as e:
                         logging.warning(e)
-                        name_url = {}
-                    urls.append(name_url)
-            data['articles_offset'] = int(data['articles_offset']) + offset
+                        return None
+        data['articles_offset'] = int(data['articles_offset']) + offset
 
     return urls
 
@@ -149,9 +144,30 @@ def write_headers_of_file():
         logging.error(e)
         return False
 
+def parse_yvkurse_link():
+    urls = []
+    try:
+        html = get_html(YVKURSE_LINK)
+        soup = BeautifulSoup(html, 'lxml')
+        author_page_articles = soup.find_all('div', class_='author-page-article')
+        if len(author_page_articles) > 0:
+            for author_page_article in author_page_articles:
+                name = author_page_article.find('span', class_='author-page-article__title').text
+                url = MAIN_LINK + author_page_article.a['href']
+                urls.append({'name': name, 'url': url})
+        else:
+            return None
+    except Exception as e:
+        logging.error(e)
+        return None
+
+    return urls
 
 def main():
     urls = get_article_links()
+    if urls is None:
+        logging.info("LOAD_MORE link return None, try parse YVKURSE_LINK")
+        urls = parse_yvkurse_link()
     if urls is not None:
         logging.info('TOTAL Articles: {}'.format(str(len(urls))))
         if write_headers_of_file():
@@ -160,6 +176,8 @@ def main():
                 res = get_text_img_article(url['url'], url['name'])
                 if res is False:
                     break
+    else:
+        print("Something wrong. LOAD_MORE and YVKURSE LINKS returned NONE")
 
 
 if __name__ == "__main__":
